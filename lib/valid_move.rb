4048cae28640fef578bb57eeba3525937b85a530
Conor Hamilton <conhamil19@gmail.com>
2018-05-01 06:27:03 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 
 def valid_move? (board, index)
-  board[index].between?(0,8) && position_taken?(array, index)
+  index.between?(0,8) && position_taken?(array, index)
 end
   
 
