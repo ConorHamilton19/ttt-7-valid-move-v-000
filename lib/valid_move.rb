984358f1277aebc920e00455de2eb200c847d56c
Conor Hamilton <conhamil19@gmail.com>
2018-05-01 06:31:07 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 
 def valid_move? (array, index)
-  index.between?(0,8) && !position_taken?
+  index.between?(0,8) && !(position_taken?(array, index))
 end
   
 
